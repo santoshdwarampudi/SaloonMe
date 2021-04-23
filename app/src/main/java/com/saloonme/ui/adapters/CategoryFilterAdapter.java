63fb24e3eafad4ea0891b1cd6eda55db7483eda2
mahesh <mahesh.reddy@zaggle.in>
2021-04-23 14:32:19 +0530

    vertical tabs

---
+++ app/src/main/java/com/saloonme/ui/adapters/CategoryFilterAdapter.java
@@ -0,0 +1,56 @@
+package com.saloonme.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.saloonme.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CategoryFilterAdapter extends RecyclerView.Adapter<CategoryFilterAdapter.ViewHolder> {
+    private Context context;
+    private boolean showBookNowOption;
+
+    public CategoryFilterAdapter(Context context,  boolean showBookNowOption) {
+        this.context = context;
+
+        this.showBookNowOption = showBookNowOption;
+    }
+
+    @NonNull
+    @Override
+    public CategoryFilterAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.category_fiter_item, parent, false);
+        return new CategoryFilterAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryFilterAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 7;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.itemCb)
+        CheckBox itemCb;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+
+}
--- app/src/main/java/com/saloonme/ui/fragments/FragmentCategory.java
+++ app/src/main/java/com/saloonme/ui/fragments/FragmentCategory.java
@@ -4,25 +4,59 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.saloonme.R;
-public class FragmentCategory extends Fragment{
+import com.saloonme.ui.adapters.CategoryFilterAdapter;
+import com.saloonme.ui.adapters.HomeServicesAdapter;
+import com.saloonme.ui.adapters.OffersHorizontalAdapter;
+import com.saloonme.ui.adapters.SaloonListAdapter;
+import com.saloonme.ui.adapters.TrendingListAdapter;
+import com.saloonme.util.CirclePagerIndicatorDecoration;
 
+import java.util.Timer;
+
+import butterknife.BindView;
+
+public class FragmentCategory extends BaseFragment{
+
+
+    @BindView(R.id.filterRv)
+    RecyclerView filterRv;
+    private CategoryFilterAdapter categoryFilterAdapter;
+
+    private View view;
     public static FragmentCategory newInstance() {
         Bundle args = new Bundle();
         FragmentCategory fragment = new FragmentCategory();
         fragment.setArguments(args);
         return fragment;
     }
-
+    @Override
+    public int getFragmentLayoutId() {
+        return R.layout.fragment_category;
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_category, container, false);
-        return v;
+        view = super.onCreateView(inflater, container, savedInstanceState);
+        initRecyclerview();
+        return view;
+    }
+
+    private void initRecyclerview() {
+
+        categoryFilterAdapter = new CategoryFilterAdapter(getContext(),  false);
+        LinearLayoutManager categoryFilterListManager = new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.VERTICAL, false);
+        filterRv.setLayoutManager(categoryFilterListManager);
+        filterRv.setAdapter(categoryFilterAdapter);
+        filterRv.setNestedScrollingEnabled(false);
     }
 }
